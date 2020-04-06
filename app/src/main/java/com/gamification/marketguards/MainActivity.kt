1d983cbb5e650c14f93440b0e49aca1248d3d334
Jan Madera <xmadera@mendelu.cz>
2020-04-06 14:56:14 +0200

    initial commit

---
+++ app/src/main/java/com/gamification/marketguards/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gamification.marketguards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
